024566d5c1ac8f8e90c98bd056132a4b8db66be2
NysaClark <69279788+NysaClark@users.noreply.github.com>
2024-02-22 15:30:22 -0700

    Add files via upload

---
+++ projectB.py
@@ -0,0 +1,54 @@
+"""
+    Nysa Clark
+    Module 05 Programming Project
+    Part B
+
+    This program ...
+"""
+
+# design a simple inheritance hierachy of your own
+
+# parent class 
+# Character
+    # name
+    # primary ablility
+    # race
+    # class
+
+class Character:
+    def __init__(self, name, race, subclass):
+        self.__name__ = name
+        self.__race__ = race
+        self.__subclass__ = subclass #? questionable attr name
+        # primary_ability is based on class ?
+        
+        # ideas & examples
+            # charisma : 1
+            # strength: 1
+            # bonus: 2
+    
+    # get name
+    
+    # get race
+        
+    # get subclass
+    
+
+
+# Barbarian
+
+
+# Bard
+
+
+# Sorcerer
+
+
+
+# create a simple class diagram for all classes 
+
+# all child class should have at least 3 things (att or methods)
+# that extend beyond the parent class
+
+# all data should be encapsulated offering minimun
+# access needed for functionality
